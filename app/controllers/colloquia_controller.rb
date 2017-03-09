7bc24989a50a42fc5f222abd8f99b4fa8f6dcbd6
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2017-03-09 12:27:16 +0300

    :construction: add private method for find and filters

--- app/controllers/colloquia_controller.rb
+++ app/controllers/colloquia_controller.rb
@@ -1,4 +1,7 @@
 class ColloquiaController < ApplicationController
+  before_action :set_colloquium, only: %i(show edit update destroy)
+  skip_before_action :authenticate_user!, only: %i(show public_index)
+
   def index
     @colloquia = Colloquium.all.order(:day)
   end
@@ -17,7 +20,13 @@ class ColloquiaController < ApplicationController
     end
   end
 
+  private
+
   def colloquium_params
     params.require(:colloquium).permit(:title, :body, :day, :bootsy_image_gallery_id)
+  end
+
+  def set_colloquium
+    @colloquium = Colloquium.find params[:id]
   end
 end
