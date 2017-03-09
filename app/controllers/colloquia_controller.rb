8415a59deb4b4416ee37bcc9ffb61233b864e04f
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2017-03-09 12:02:25 +0300

    :construction: add create action

--- app/controllers/colloquia_controller.rb
+++ app/controllers/colloquia_controller.rb
@@ -6,4 +6,18 @@ class ColloquiaController < ApplicationController
   def new
     @colloquium = Colloquium.new
   end
+
+  def create
+    @colloquium = Colloquium.new colloquium_params
+
+    if @colloquium.save
+      redirect_to colloquia_path, notice: 'Colloquium was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def colloquium_params
+    params.require(:colloquium).permit(:title, :body, :day, :bootsy_image_gallery_id)
+  end
 end
